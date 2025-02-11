0eb6f2a1c36dcb751ab7d3360a5503e12810518e
hTKzmak <ineedrtx@gmail.com>
2025-02-11 17:26:22 +0300

    updated app.scss

--- src/pages/ContentPage/index.tsx
+++ src/pages/ContentPage/index.tsx
@@ -8,11 +8,11 @@ type PageType = {
 export default function ContentPage({type}: PageType) {
     return (
         <div className="content">
-            <div className="music_content">
+            <div className="musicContent">
                 <h3>{type === 'homepage' ? 'Recommended music' : 'Popular music'}</h3>
                 <MusicList/>
             </div>
-            <div className="performers_content">
+            <div className="performersContent">
                 <h3>{type === 'homepage' ? 'Recommended artists' : 'Popular artists'}</h3>
                 <PerformerList />
             </div>
