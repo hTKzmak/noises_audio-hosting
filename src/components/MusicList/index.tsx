9502506c9ea511d47d919d07376756b799908d31
hTKzmak <ineedrtx@gmail.com>
2025-02-14 20:33:29 +0300

    updated data for music list

--- src/components/MusicList/index.tsx
+++ src/components/MusicList/index.tsx
@@ -1,14 +1,21 @@
+import { useEffect, useState } from 'react'
 import MusicItem from '../MusicItem'
 import style from './MusicList.module.scss'
 
 export default function MusicList({ data, scrollMusicsRef }: any) {
+    const [onlyMusic, setOnlyMusic] = useState([]);
 
-    console.log(data)
+    useEffect(() => {
+        if (data && data.length > 0) {
+            const newData = data.flatMap((item: any) => item.music);
+            setOnlyMusic(newData);
+        }
+    }, [data]);
 
     return (
         <div className={style.musicList} ref={scrollMusicsRef}>
-            {data.map((elem: any) => (
-                <MusicItem key={elem.id} id={elem.id} title={elem.title} artist={elem.artist} artwork={elem.artwork} url={elem.url}/>
+            {onlyMusic.map((elem: any) => (
+                <MusicItem key={elem.id} id={elem.id} title={elem.title} artist={elem.artist} artwork={elem.artwork} url={elem.url} />
             ))}
         </div>
     )
